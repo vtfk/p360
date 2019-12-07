ffa69598009f332019ccb356db91f95df1f15259
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-07 15:54:21 +0100

    Implements ContactService

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const CaseService = require('./lib/case-service')
+const ContactService = require('./lib/contact-service')
 
 module.exports = options => ({
-  CaseService: CaseService(options)
+  CaseService: CaseService(options),
+  ContactService: ContactService(options)
 })
