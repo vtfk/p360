80cc88d5586589e6885438e0df9398c885e1840f
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-08 07:12:48 +0100

    Exports all the modules

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const CaseService = require('./lib/case-service')
 const ContactService = require('./lib/contact-service')
+const DocumentService = require('./lib/document-service')
+const FileService = require('./lib/file-service')
+const UserService = require('./lib/user-service')
 
 module.exports = options => ({
   CaseService: CaseService(options),
-  ContactService: ContactService(options)
+  ContactService: ContactService(options),
+  DocumentService: DocumentService(options),
+  FileService: FileService(options),
+  UserService: UserService(options)
 })
